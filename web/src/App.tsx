b89f64e6f4fac6c6d461d7a2da2c5945dabbec5f
alvdelci <rafithydev@gmail.com>
2022-09-12 00:14:48 -0300

    feat: Configurando o frontend web

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+function App() {
+  return (<>
+    <Button title="Send 1" />
+    <Button title="Send 2" />
+    <Button title="Send 3" />
+  </>)
+}
+
+export default App
